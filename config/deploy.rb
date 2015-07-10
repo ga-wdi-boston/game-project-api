6f3c793d5f760694730fe76d559abddcdd372cd0
Jeffrey Horn <jeffh@generalassemb.ly>
2015-07-10 10:43:18 -0400

    Set ruby-version for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,6 +34,8 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+set :chruby_ruby, 'ruby-2.2.2'
+
 namespace :deploy do
 
   desc 'Restart application'
