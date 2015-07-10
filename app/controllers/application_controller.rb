5253f5da91cb95ec3c080c7c252b9e92c079e33b
Jeffrey Horn <jeffh@generalassemb.ly>
2015-07-10 15:39:12 -0400

    Do not force SSL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token, if: :json_request?
-  force_ssl if: :ssl_configured?
+  # force_ssl if: :ssl_configured?
 
   protected
 
