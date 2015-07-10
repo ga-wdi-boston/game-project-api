34b5837fcc50b8d2821229130c51c30cf2a3526c
Jeffrey Horn <jeffh@generalassemb.ly>
2015-07-10 10:41:33 -0400

    The comment is a lie. There was no subdir under /var/www

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:ga-wdi-boston/rails-tic-tac-toe-api.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/var/www/rails-tic-tac-toe-api'
 
 # Default value for :scm is :git
 # set :scm, :git
