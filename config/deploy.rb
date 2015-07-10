a08de8a5971ea6548e25f1b6d1d67b8a61e19c3e
Jeffrey Horn <jeffh@generalassemb.ly>
2015-07-10 10:42:26 -0400

    Don't link config/database.yml since it is in source control

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/var/www/rails-tic-tac-toe-api'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+# set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
