9e7e7997a009c605fde949b013ad460b02440708
Bernard Lee <bernardlee82@gmail.com>
2017-03-26 16:40:42 -0400

    Create a migration to add additional constraints
    Add the following constraints to the appropriate columns in the "games"
    table.
    - 3 <= m <= 20
    - 3 <= n <= 20
    - 3 <= k <= min(m, n)
    - cells's length = m * n

---
+++ db/migrate/003_add_additional_constraints.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal:true
+# rubocop:disable Metrics/MethodLength
+class AddAdditionalConstraints < ActiveRecord::Migration
+  def change
+    reversible do |direction|
+      direction.up do
+        execute <<-SQL.squish
+          ALTER TABLE games
+          ADD CONSTRAINT m
+          CHECK (m >= 3 AND m <= 20),
+          ADD CONSTRAINT n
+          CHECK (n >= 3 AND n <= 20),
+          ADD CONSTRAINT k
+          CHECK (k >= 3 AND k <= LEAST(m, n)),
+          ADD CONSTRAINT cells
+          CHECK (array_length(cells, 1) = m * n)
+        SQL
+      end
+
+      direction.down do
+        execute <<-SQL.squish
+          ALTER TABLE games
+          DROP CONSTRAINT m,
+          DROP CONSTRAINT n,
+          DROP CONSTRAINT k
+        SQL
+      end
+    end
+  end
+end
