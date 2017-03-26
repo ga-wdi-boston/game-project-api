d7e18c5721c6498f60128017b4d36e0bb96aa1fa
Bernard Lee <bernardlee82@gmail.com>
2017-03-26 16:42:55 -0400

    Update database schema
    Run "rails db:migrate".

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 3) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 2) do
     t.integer "m",           default: 3,     null: false
     t.integer "n",           default: 3,     null: false
     t.integer "k",           default: 3,     null: false
-    t.string  "cells",                                    array: true
-    t.boolean "over",        default: false, null: false
+    t.string  "cells",                       null: false, array: true
+    t.boolean "over",        default: false
   end
 
   create_table "users", force: :cascade do |t|
