0a7f2edeaf845f9c0e6e9af597d7a634478bdafa
Bernard Lee <bernardlee82@gmail.com>
2017-04-16 19:44:59 -0400

    Rename application module
    Change RailsApiTemplate to GameProjectAPI.

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ require 'action_controller/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsApiTemplate
+module GameProjectAPI
   # :nodoc:
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified
