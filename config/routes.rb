e1ff1b7f1a92bd89436df169da4a1ebeaf782c34
Bernard Lee <bernardlee82@gmail.com>
2017-04-20 05:13:51 -0400

    Add routes for two new games
    - Add "/gomoku" and "/tic-tac-toe" routes for the respective games.
    - Update "/games" for backward compatiblity.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,37 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :games, except: :destroy, controller: 'mnk_games'
+  # m,n,k-game routes.
+
+  # /mnk-games.
+  # Creates an m,n,k-game.  m, n, and k need to be specified.
+  resources :mnk_games, except: :destroy, path: 'mnk-games'
+
+  # /games.
+  # Creates a 3,3,3-game for backward compatiblity.
+  resources :games, except: %i[create destroy], controller: 'mnk_games'
+  post '/games', to: 'mnk_games#create',
+                 defaults: { game: { m: 3, n: 3, k: 3 } }
+
+  # /gomoku.
+  # Creates a 15,15,3-game.
+  resources :gomoku, except: %i[create destroy], controller: 'mnk_games'
+  post '/gomoku', to: 'mnk_games#create',
+                  defaults: { game: { m: 15, n: 15, k: 5 } }
+
+  # /tic-tac-toe.
+  # Creates a 3,3,3-game.
+  resources :tic_tac_toe, except: %i[create destroy],
+                          controller: 'mnk_games',
+                          path: 'tic-tac-toe'
+  post '/tic-tac-toe', to: 'mnk_games#create',
+                       defaults: { game: { m: 3, n: 3, k: 3 } }
+
   # this is the streaming interface
+  get '/mnk_games/:id/watch' => 'mnk_games#watch'
   get '/games/:id/watch' => 'mnk_games#watch'
+  get '/gomoku/:id/watch' => 'mnk_games#watch'
+  get '/tic-tac-toe/:id/watch' => 'mnk_games#watch'
 
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
